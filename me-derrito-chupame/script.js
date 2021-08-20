cb33bb048e7c71bd9bad694db1ad537b19bbe0bc
LuisAngelMaciel <69272932+hiLuisXiib@users.noreply.github.com>
2021-08-20 03:21:59 -0500

    Add files via upload

---
+++ me-derrito-chupame/script.js
@@ -0,0 +1,15 @@
+/*  Fullscreen btn  */
+let fullscreen;
+let fsEnter = document.getElementById('fullscr');
+fsEnter.addEventListener('click', function(e) {
+    e.preventDefault();
+    if (!fullscreen) {
+        fullscreen = true;
+        document.documentElement.requestFullscreen();
+        fsEnter.innerHTML = "⇲";
+    } else {
+        fullscreen = false;
+        document.exitFullscreen();
+        fsEnter.innerHTML = "⇱";
+    }
+});
